82d178dc4f9c6fa74642d635b9e87503f932af47
halitm4m <skfstudio9@gmail.com>
2020-12-06 05:34:16 +0300

    Naile

---
+++ Popcorn/Assets/FareDONUSU.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FareDONUSU : MonoBehaviour
+{
+    [Range(50, 500)]
+    public float sens;
+
+    public Transform body;
+
+    float xrot = 0f;
+
+    private void Update()
+    {
+        float rotx = Input.GetAxisRaw("Mouse X") * sens * Time.deltaTime;
+        float roty = Input.GetAxisRaw("Mouse Y") * sens * Time.deltaTime;
+        xrot -= roty;
+        xrot = Mathf.Clamp(xrot, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xrot, 0f, 0f);
+
+        body.Rotate(Vector3.up * rotx);
+    }
+
+}
