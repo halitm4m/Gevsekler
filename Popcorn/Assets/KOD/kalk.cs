4bf76d5f034fa028c73f6058b2c9b2967266b305
halitm4m <skfstudio9@gmail.com>
2020-12-06 16:01:32 +0300

    şakşuka

---
+++ Popcorn/Assets/KOD/kalk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kalk : MonoBehaviour
+{
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("ko", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetBool("ko", false);
+        }
+    }
+}
